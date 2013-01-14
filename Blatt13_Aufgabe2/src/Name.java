c487ceb43f450cf3367ca4bbb18e4804046d5b74
ThiloFalkenstein <t.falkenstein@fh-bingen.de>
2013-01-14 11:52:14 +0100

    Create Project

---
+++ Blatt13_Aufgabe2/src/Name.java
@@ -0,0 +1,16 @@
+
+public class Name {
+
+	String first, last, title;
+	
+	Name( String first, String last, String title ) {
+	this.first = first;
+	this.last = last;
+	this.title = title;
+	}
+	
+	Name( String first, String last ) {
+	this( first, last, "" );
+	}
+	
+}//end class
